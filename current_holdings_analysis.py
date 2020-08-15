8f38b4a409bec820db6e926ce59f67741c6bbed4
sivacharansrc <sivacharansrc@gmail.com>
2020-08-15 16:58:25 -0500

    updated current_holdings_analysis file

--- current_holdings_analysis.py
+++ current_holdings_analysis.py
@@ -6,16 +6,18 @@ import numpy as np
 
 ### CAPTURE CURRENT HOLDINGS IN A LIST ###
 
-my_current_holdings = ["VTI FZROX FSKAX VOO IVV FXAIX FNILX VGT FTEC VHT FHLC IHI SMH XSD ARKK ARKW ARKF ARKQ BND AGG FNBGX"]
+my_current_holdings = ["VTI FZROX FSKAX VOO IVV FXAIX FNILX VGT FTEC XITK VHT FHLC IHI XHE SMH XSD ARKK ARKW ARKF ARKQ ARKG SLV GLDM FPBFX FIVFX BND AGG FNBGX"]
 
 ### DOWNLOAD DATA FROM YFINANCE ###
 
-df = yf.download("VTI FZROX FSKAX VOO IVV FXAIX FNILX VGT FTEC VHT FHLC IHI SMH XSD ARKK ARKW ARKF ARKQ BND AGG FNBGX", start="1990-01-01")
+start_date = str(int(datetime.today().strftime('%Y')) - 10)  + '-01-01' # Pulling 10 Years of data
+
+df = yf.download("VTI FZROX FSKAX VOO IVV FXAIX FNILX VGT FTEC VHT FHLC IHI SMH XSD ARKK ARKW ARKF ARKQ BND AGG FNBGX", start=start_date)
 df = df.unstack().reset_index()
 df.columns = ['col_name', 'Ticker', 'Date', 'Value']
 df = df.pivot_table(values='Value', index=['Ticker', 'Date'],columns='col_name').reset_index()
 df = df.sort_values(['Ticker', 'Date'])
 df.columns.name = None
 
-df[df['Ticker']=='FNBGX']
+df.to_csv(r"/home/my-portfolio-analysis/historical_quotes.csv", index=False)
 
